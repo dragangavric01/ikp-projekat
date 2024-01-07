00ecccf55bb2be5012f2ca915dd1c0c2b54ec566
Luka Išpanović <ispanovic.luka@gmail.com>
2024-01-07 15:24:00 +0100

    Dodat non blocking preko multipleksiranja, neogranicen broj klijenata i publish subscribe select ekran

--- PubSub/Broker/broker.cpp
+++ PubSub/Broker/broker.cpp
@@ -1,66 +1,26 @@
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
+#include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 #pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
 
-#define SERVER_PORT 16050
+#define SERVER_PORT 27016
 #define BUFFER_SIZE 256
-#define SUBSCRIBER_IP_ADDRESS "127.0.0.1"
-#define SUBSCRIBER_PORT 20000
 
 int main() {
-    SOCKET listenSocket = INVALID_SOCKET;
-    SOCKET acceptedSocket = INVALID_SOCKET;
-    int iResult;
-    char dataBuffer[BUFFER_SIZE];
     WSADATA wsaData;
+    SOCKET listenSocket = INVALID_SOCKET;
+    SOCKET clientSockets[FD_SETSIZE];
+    int clientCount = 0;
+    char dataBuffer[BUFFER_SIZE];
 
+    // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("WSAStartup failed with error: %d\n", WSAGetLastError());
         return 1;
     }
 
-    // Creating a socket for the subscriber
-    SOCKET secondServerSocket = INVALID_SOCKET;
-    sockaddr_in secondServerAddress;
-    secondServerAddress.sin_family = AF_INET;
-    secondServerAddress.sin_addr.s_addr = inet_addr(SUBSCRIBER_IP_ADDRESS);
-    secondServerAddress.sin_port = htons(SUBSCRIBER_PORT);
-
-    secondServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (secondServerSocket == INVALID_SOCKET) {
-        printf("socket for subscriber failed with error: %ld\n", WSAGetLastError());
-        closesocket(acceptedSocket);
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    if (connect(secondServerSocket, (SOCKADDR*)&secondServerAddress, sizeof(secondServerAddress)) == SOCKET_ERROR) {
-        printf("Unable to connect to subscriber.\n");
-        closesocket(secondServerSocket);
-        closesocket(acceptedSocket);
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Connected to subscriber.\n");
-
-    sockaddr_in serverAddress;
-    memset((char*)&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(SERVER_PORT);
-
+    // Create a SOCKET for listening for incoming connection requests
     listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (listenSocket == INVALID_SOCKET) {
         printf("socket failed with error: %ld\n", WSAGetLastError());
@@ -68,72 +28,89 @@ int main() {
         return 1;
     }
 
-    iResult = bind(listenSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    if (iResult == SOCKET_ERROR) {
+    // Setup the TCP listening socket
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(SERVER_PORT);
+
+    if (bind(listenSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
         printf("bind failed with error: %d\n", WSAGetLastError());
         closesocket(listenSocket);
         WSACleanup();
         return 1;
     }
 
-    iResult = listen(listenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
+    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
         printf("listen failed with error: %d\n", WSAGetLastError());
         closesocket(listenSocket);
         WSACleanup();
         return 1;
     }
 
-    printf("Broker waiting for incoming publisher connection.\n");
+    printf("Broker is now running. Waiting for clients...\n");
 
-    sockaddr_in clientAddr;
-    int clientAddrSize = sizeof(struct sockaddr_in);
+    fd_set readfds;
+    int maxSocketDescriptor = listenSocket;
 
-    acceptedSocket = accept(listenSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
-    if (acceptedSocket == INVALID_SOCKET) {
-        printf("accept failed with error: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
+    while (true) {
+        FD_ZERO(&readfds);
+        FD_SET(listenSocket, &readfds);
 
-    printf("\nPublisher connection accepted. Address: %s : %d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-
-    do {
-        iResult = recv(acceptedSocket, dataBuffer, BUFFER_SIZE, 0);
-        if (iResult > 0) {
-            dataBuffer[iResult] = '\0';
-            printf("Message received from publisher: %s\n", dataBuffer);
-
-            // Sending the received message to the subscriber
-            int sendResult = send(secondServerSocket, dataBuffer, strlen(dataBuffer), 0);
-            if (sendResult == SOCKET_ERROR) {
-                printf("Failed to send message to the subscriber: %d\n", WSAGetLastError());
-                break;
-            }
-            printf("Message sent to the subscriber.\n");
+        for (int i = 0; i < clientCount; ++i) {
+            FD_SET(clientSockets[i], &readfds);
         }
-        else if (iResult == 0) {
-            printf("Connection with publisher closed.\n");
+
+        int activity = select(0, &readfds, NULL, NULL, NULL);
+        if (activity == SOCKET_ERROR) {
+            printf("select error\n");
             break;
         }
-        else {
-            if (WSAGetLastError() == WSAEWOULDBLOCK) {
+
+        if (FD_ISSET(listenSocket, &readfds)) {
+            SOCKET newSocket = accept(listenSocket, NULL, NULL);
+            if (newSocket == INVALID_SOCKET) {
+                printf("accept failed with error: %d\n", WSAGetLastError());
             }
             else {
-                printf("recv failed with error: %d\n", WSAGetLastError());
-                break;
+                printf("New client connected\n");
+                clientSockets[clientCount++] = newSocket;
+                unsigned long mode = 1; // Set the new socket to non-blocking
+                ioctlsocket(newSocket, FIONBIO, &mode);
             }
         }
 
-        Sleep(1000);
+        for (int i = 0; i < clientCount; ++i) {
+            if (FD_ISSET(clientSockets[i], &readfds)) {
+                int bytesRead = recv(clientSockets[i], dataBuffer, BUFFER_SIZE, 0);
+                if (bytesRead == SOCKET_ERROR) {
+                    int error = WSAGetLastError();
+                    if (error != WSAEWOULDBLOCK) {
+                        printf("Error in recv on socket %d: %d\n", clientSockets[i], error);
+                    }
+                }
+                else if (bytesRead == 0) {
+                    printf("Client disconnected\n");
+                    closesocket(clientSockets[i]);
+                    if (i < clientCount - 1) {
+                        clientSockets[i] = clientSockets[clientCount - 1];
+                    }
+                    --clientCount;
+                }
+                else {
+                    dataBuffer[bytesRead] = '\0';
+                    printf("Message from client: %s\n", dataBuffer);
+                    for (int j = 0; j < clientCount; ++j) {
+                        if (i != j) {
+                            send(clientSockets[j], dataBuffer, bytesRead, 0);
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-    } while (true);
-
-    closesocket(secondServerSocket);
-    closesocket(acceptedSocket);
     closesocket(listenSocket);
     WSACleanup();
-
     return 0;
 }
--- PubSub/Client/Client.cpp
+++ PubSub/Client/Client.cpp
@@ -1,16 +1,3 @@
-#include <stdio.h>
-
-int main() {
-
-	return 0;
-}
-
-
-// OVO DOLE JE KOD KOJI JE BIO U publisher.cpp !!!!!!!!!!!!!!!!!!!!!!!
-// OVO DOLE JE KOD KOJI JE BIO U publisher.cpp !!!!!!!!!!!!!!!!!!!!!!!
-// OVO DOLE JE KOD KOJI JE BIO U publisher.cpp !!!!!!!!!!!!!!!!!!!!!!!
-
-/*
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
 
@@ -25,13 +12,15 @@ int main() {
 #pragma comment (lib, "AdvApi32.lib")
 
 #define SERVER_IP_ADDRESS "127.0.0.1"
-#define SERVER_PORT 16050
+#define SERVER_PORT 27016
 #define BUFFER_SIZE 256
+
+void publisher(SOCKET connectSocket);
+void subscriber(SOCKET connectSocket);
 
 int main() {
     SOCKET connectSocket = INVALID_SOCKET;
     WSADATA wsaData;
-    char dataBuffer[BUFFER_SIZE];
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         printf("WSAStartup failed with error: %d\n", WSAGetLastError());
@@ -60,123 +49,72 @@ int main() {
     printf("Connected to broker.\n");
 
     while (true) {
-        printf("\nEnter a message to send to the broker: ");
-        gets_s(dataBuffer, BUFFER_SIZE);
+        int choice;
 
-        int iResult = send(connectSocket, dataBuffer, strlen(dataBuffer), 0);
-        if (iResult == SOCKET_ERROR) {
-            printf("send failed with error: %d\n", WSAGetLastError());
+        printf("Menu:\n");
+        printf("1. Publisher\n");
+        printf("2. Subscriber\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+
+        scanf_s("%d", &choice);
+
+        if (choice == 1) {
+            publisher(connectSocket);
+        }
+        else if (choice == 2) {
+            subscriber(connectSocket);
+        }
+        else if (choice == 3) {
+            shutdown(connectSocket, SD_BOTH);
             closesocket(connectSocket);
             WSACleanup();
-            return 1;
+            return 0;
         }
-
-        printf("Message successfully sent.");
-
-        printf("\nType quit to close program, or any other message to continue: ");
-        gets_s(dataBuffer, BUFFER_SIZE);
-        if (!strcmp(dataBuffer, "quit"))
-            break;
+        else {
+            printf("Invalid choice. Please enter either 1, 2 or 3.\n");
+        }
     }
-
     shutdown(connectSocket, SD_BOTH);
     closesocket(connectSocket);
     WSACleanup();
-
-    return 0;
+	return 0;
 }
-*/
+
+void publisher(SOCKET connectSocket) {
+    char dataBuffer[BUFFER_SIZE];
+
+        printf("\nEnter a message to send to the broker: ");
+        if (scanf_s(" %[^\n]", dataBuffer, BUFFER_SIZE) != 1) {
+            printf("Error reading input.\n");
+            closesocket(connectSocket);
+            WSACleanup();
+        }
+
+        if (strlen(dataBuffer) > 0) {
+            int iResult = send(connectSocket, dataBuffer, strlen(dataBuffer), 0);
+            if (iResult == SOCKET_ERROR) {
+                printf("send failed with error: %d\n", WSAGetLastError());
+                closesocket(connectSocket);
+                WSACleanup();
+            }
+            printf("Message successfully sent.\n");
+        }
+        else {
+            printf("No message entered.\n");
+        }
+}
 
 
-
-
-// OVO DOLE JE KOD KOJI JE BIO U subscriber.cpp !!!!!!!!!!!!!!!!!!!!!!!
-// OVO DOLE JE KOD KOJI JE BIO U subscriber.cpp !!!!!!!!!!!!!!!!!!!!!!!
-// OVO DOLE JE KOD KOJI JE BIO U subscriber.cpp !!!!!!!!!!!!!!!!!!!!!!!
-
-/*
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
-#define WIN32_LEAN_AND_MEAN
-
-#include <windows.h>
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-#pragma comment (lib, "Ws2_32.lib")
-#pragma comment (lib, "Mswsock.lib")
-#pragma comment (lib, "AdvApi32.lib")
-
-#define SERVER_PORT 20000
-#define BUFFER_SIZE 256
-
-int main() {
-    SOCKET listenSocket = INVALID_SOCKET;
-    SOCKET acceptedSocket = INVALID_SOCKET;
+void subscriber(SOCKET connectSocket) {
     int iResult;
     char dataBuffer[BUFFER_SIZE];
-    WSADATA wsaData;
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
-        return 1;
-    }
-
-    sockaddr_in serverAddress;
-    memset((char*)&serverAddress, 0, sizeof(serverAddress));
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    serverAddress.sin_port = htons(SERVER_PORT);
-
-    listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (listenSocket == INVALID_SOCKET) {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        WSACleanup();
-        return 1;
-    }
-
-    iResult = bind(listenSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    if (iResult == SOCKET_ERROR) {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    iResult = listen(listenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
-        printf("listen failed with error: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("Subscriber waiting for broker to connect...\n");
-
-    sockaddr_in clientAddr;
-    int clientAddrSize = sizeof(struct sockaddr_in);
-
-    acceptedSocket = accept(listenSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
-    if (acceptedSocket == INVALID_SOCKET) {
-        printf("accept failed with error: %d\n", WSAGetLastError());
-        closesocket(listenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    printf("\nBroker connection accepted. Address: %s : %d\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-
-    unsigned long mode = 1; //non-blocking mode
-    iResult = ioctlsocket(acceptedSocket, FIONBIO, &mode);
-    if (iResult != NO_ERROR)
-        printf("ioctlsocket failed with error: %ld\n", iResult);
-
     do {
-        iResult = recv(acceptedSocket, dataBuffer, BUFFER_SIZE, 0);
+        iResult = recv(connectSocket, dataBuffer, BUFFER_SIZE, 0);
         if (iResult > 0) {
             dataBuffer[iResult] = '\0';
-            printf("Message received from broker : %s\n", dataBuffer);
+            printf("Message received from broker: %s\n", dataBuffer);
+            break;
         }
         else if (iResult == 0) {
             printf("Connection with broker closed.\n");
@@ -184,6 +122,7 @@ int main() {
         }
         else {
             if (WSAGetLastError() == WSAEWOULDBLOCK) {
+                // No data available to receive
             }
             else {
                 printf("recv failed with error: %d\n", WSAGetLastError());
@@ -194,11 +133,4 @@ int main() {
         Sleep(1000);
 
     } while (true);
-
-    closesocket(listenSocket);
-    closesocket(acceptedSocket);
-    WSACleanup();
-
-    return 0;
 }
-*/
